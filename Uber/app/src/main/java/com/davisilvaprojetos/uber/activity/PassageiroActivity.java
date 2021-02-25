d2ca77c349ab7fb54d9bc8a5c1a2bc0636b6fb5a
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-02-25 15:20:05 -0300

    -Recuperando localização do usuário

--- Uber/app/src/main/java/com/davisilvaprojetos/uber/activity/PassageiroActivity.java
+++ Uber/app/src/main/java/com/davisilvaprojetos/uber/activity/PassageiroActivity.java
@@ -1,5 +1,11 @@
 package com.davisilvaprojetos.uber.activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.davisilvaprojetos.uber.config.ConfiguracaoFirebase;
@@ -7,6 +13,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,6 +22,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,6 +36,8 @@ public class PassageiroActivity extends AppCompatActivity
 
     private GoogleMap mMap;
     private FirebaseAuth autenticacao;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +59,43 @@ public class PassageiroActivity extends AppCompatActivity
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //Recuperar a localização do usuário
+        recuperarLocalizacaoUsuario();
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void recuperarLocalizacaoUsuario() {
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                //Recuperar latitude e longitude
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng meuLocal = new LatLng(latitude, longitude);
+                mMap.clear();
+                mMap.addMarker(
+                        new MarkerOptions()
+                                .position(meuLocal)
+                                .title("Meu local")
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.usuario))
+                );
+                mMap.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(meuLocal, 20)
+                );
+            }
+        };
+
+        //Solicitar atualizações de localização
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    10000,
+                    10,
+                    locationListener
+            );
+        }
+
     }
 
     @Override
